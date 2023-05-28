a4f6df6d279945d4cc23507047bc944ac0c214c1
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-05-28 16:17:16 -0700

    created user manager

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 # Create your models here.
 
 class User(AbstractUser):
@@ -7,5 +8,12 @@ class User(AbstractUser):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    # custom user manager
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
