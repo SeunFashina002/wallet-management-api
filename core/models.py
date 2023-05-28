20b64ea0d2f8240a9bf05bf3956eb3a022a4a7b3
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-05-28 15:45:20 -0700

    created user model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser):
+    # additional fields 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
