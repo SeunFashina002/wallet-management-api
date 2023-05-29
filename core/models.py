5c280f3b30686651112305c1267825ef9c37b1e0
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-05-29 10:49:25 -0700

    added voucher and transactions model

--- core/models.py
+++ core/models.py
@@ -22,3 +22,39 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+# other models
+
+class Voucher(models.Model):
+    code = models.CharField(max_length=9, unique=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    is_redeemed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.code}"
+    
+
+
+class Transactions(models.Model):    
+    # types of transactions
+    VOUCHER = 'voucher'
+    SENT = 'sent'
+    RECEIVED = 'received'
+    TRANSACTION_TYPES = [
+        (VOUCHER, 'Vouchers'),
+        (SENT, 'Sent'),
+        (RECEIVED, 'Received'),
+    ]
+
+    voucher = models.ForeignKey(Voucher, on_delete=models.SET_NULL, null=True)
+    transaction_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    transaction_type = models.CharField(choices=TRANSACTION_TYPES, max_length=15)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.transaction_type}"
