3b2079358d632f306e44528a21a34f597a128584
Seun Fashina <fashinaoluwaseun36@gmail.com>
2023-05-29 17:45:23 -0700

    set debug value

--- wallet/settings.py
+++ wallet/settings.py
@@ -28,10 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('DEBUG') is not None:
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = False
 
 
 
